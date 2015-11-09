ca1737bfc96d812556d5ef1f97342469da3ee230
greg <dev.pollux@gmail.com>
2015-11-09 16:04:43 +0100

    initial Commit : Fonctionne avec une listBox.

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        class Oiseau
+        {
+            public string nom {get; set; }
+            public Int32 envergure { get; set; }
+            
+
+            public Oiseau()
+            {
+
+            }
+            public Oiseau(string nom, Int32 envergure)
+            {
+                this.nom = nom;
+                this.envergure = envergure;
+            }
+        }
+
+        List<Oiseau> vivarium = new List<Oiseau>();
+        ObservableCollection<Oiseau> liveArium = new ObservableCollection<Oiseau>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            liveArium.Add(new Oiseau("canard mandarin", 12));
+            liveArium.Add(new Oiseau("canard breton", 22));
+            liveArium.Add(new Oiseau("albatros", 90));
+            lbVivarium.ItemsSource = liveArium;
+
+
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Oiseau o = new Oiseau(textNom.Text , Int32.Parse(textEnvergure.Text));
+            liveArium.Add(o);
+            
+
+        }
+
+
+        //Remove supprime le dernier element du vivarium
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
+        {
+            if (liveArium.Count > 0)
+            {
+                string btnText = liveArium.Last().nom;
+                liveArium.Remove(liveArium.Last());
+
+            }
+        }
+
+        private void lbVivarium_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lbVivarium.SelectedItem != null)
+            {
+                txtDetails.Text = (lbVivarium.SelectedItem as Oiseau).envergure.ToString();
+
+            }
+        }
+    }
+}
